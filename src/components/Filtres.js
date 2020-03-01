38b1b77a815d823519744c1943534abadbbb6fdf
Kriddina Megnafi <kriddina.megnafi@gmail.com>
2020-03-01 15:40:43 +0100

    Filtre_style

---
+++ src/components/Filtres.js
@@ -0,0 +1,52 @@
+import React, { useContext, useState } from "react";
+import { Text, View, TouchableOpacity } from "react-native";
+import styles from "../../styles";
+
+//--- Icônes
+import { Ionicons, Entypo, FontAwesome } from "@expo/vector-icons";
+
+//--- Component
+import Filtre from "../components/Filtre";
+
+const Filtres = () => {
+  [key, useKey] = useState(false);
+
+  //--- Return
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        paddingLeft: 5,
+        paddingRight: 5,
+        backgroundColor: "#d4e6f1"
+      }}
+    >
+      <View style={{ width: "25%" }}>
+        <Filtre
+          title1="Remise"
+          title2=" des clés"
+          service="key"
+          color="brown"
+        />
+      </View>
+      <View style={{ width: "25%" }}>
+        <Filtre
+          title1="Ménage"
+          title2="et linge"
+          service="trash"
+          color="green"
+        />
+      </View>
+
+      <View style={{ width: "25%" }}>
+        <Filtre title1="Travaux" title2="divers" service="tools" color="blue" />
+      </View>
+
+      <View style={{ width: "25%" }}>
+        <Filtre title1="Filtres" title2="Distance" service="menu" />
+      </View>
+    </View>
+  );
+};
+
+export default Filtres;

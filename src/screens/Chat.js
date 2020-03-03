8161f38c593aa407a8e6e252d01f5acadfd6f04a
Ridha <rbenrhouma2@gmail.com>
2020-03-03 11:18:10 +0100

    class to function

--- src/screens/Chat.js
+++ src/screens/Chat.js
@@ -1,48 +1,38 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { GiftedChat } from "react-native-gifted-chat";
 
-class ChatScreen extends Component {
-  state = {
-    messages: []
+const ChatScreen = () => {
+  const [myMessages, setMyMessages] = useState([]);
+  useEffect(() => {
+    myMessages.push({
+      _id: 1,
+      text: "Salut M. le proprio",
+      createdAt: new Date(),
+      user: {
+        _id: 2,
+        name: "React Native",
+        avatar: "https://placeimg.com/140/140/any"
+      }
+    });
+  }, []);
+
+  const onSend = message => {
+    let msg = [...myMessages];
+    msg.unshift(message[0]);
+    setMyMessages(msg);
   };
 
-  componentDidMount() {
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: "Salut M. le proprio",
-          createdAt: new Date(),
-
-          user: {
-            _id: 2,
-            name: "React Native",
-            avatar: "https://placeimg.com/140/140/any"
-          }
-        }
-      ]
-    });
-  }
-
-  onSend(messages = []) {
-    this.setState(previousState => ({
-      messages: GiftedChat.append(previousState.messages, messages)
-    }));
-  }
-
-  render() {
-    return (
-      <GiftedChat
-        messages={this.state.messages}
-        onSend={messages => this.onSend(messages)}
-        placeholder="Messace text"
-        user={{
-          _id: 1,
-          name: "Ridha"
-        }}
-      />
-    );
-  }
-}
+  return (
+    <GiftedChat
+      messages={myMessages}
+      onSend={messages => onSend(messages)}
+      placeholder="Messace text"
+      user={{
+        _id: 1,
+        name: "Ridha"
+      }}
+    />
+  );
+};
 
 export default ChatScreen;
